ab051c6c9800359e2d2533b8c7e92c1e1e2b6f69
shohan12727 <aishohan001@gmail.com>
2025-04-15 15:32:16 +0600

    got question

--- src/component/Accordian.jsx
+++ src/component/Accordian.jsx
@@ -1,26 +1,43 @@
-import React, {useState} from 'react';
+// single selection accordiam
+
+import React, { useState } from 'react';
+import data from './data';
 
 const Accordian = () => {
 
     const [selected, setSelected] = useState(null);
-    c
-    
 
-    function handleSingleSelection (getCurrentId) {
-        setSelected (getCurrentId === selected ? null : getCurrentId);
+    function handleSingleSelection(getCurrentId) {
+        console.log(getCurrentId);
+
     }
 
-   
-
-
-
-
     return (
-        <div>
+        <div className='wrapper' >
+            <div className="accordian">
+                {
+                    data && data.length > 0 ?
+                        data.map(dataItem => <div className="item">
+                            <div onClick={() => handleSingleSelection(dataItem.id)}>
+                                <div className="title">
+                                    <h3>{dataItem.question}</h3>
+                                    <span>+</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        )
+
+
+                        : <div> No data found </div>
+                }
+
+
+            </div>
 
 
 
-            
+
         </div>
     );
 };
